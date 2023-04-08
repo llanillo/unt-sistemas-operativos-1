28d04a6b38c9a33e53b3d97507a5fa515f6280fe
Octavio Llanillo <llanillo4@hotmail.com>
2023-04-08 20:12:11 -0300

    Finaliza TP1 Punto 6

--- TP1/TP1-Punto6.c
+++ TP1/TP1-Punto6.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
     FILE *archivo;
-    char caracter;
-
-    archivo = fopen("texto.txt", "r");
-
-    if (archivo == NULL) {
-        printf("Archivo no existente");
-        return EXIT_FAILURE;
-    } else {
-        printf("\nEl contenido del archivo es \n");
-        while ((caracter = fgetc(archivo)) != EOF) {
-            printf("%c", caracter);
-        }
-    }
-
-    return EXIT_SUCCESS;
+    char linea1[100], linea2[100];
+    
+    // Abrir el archivo en modo de lectura
+    archivo = fopen("archivo.txt", "r");
+    
+    // Leer la primera línea y almacenarla en la variable "linea1"
+    fgets(linea1, 100, archivo);
+    
+    // Leer la segunda línea y almacenarla en la variable "linea2"
+    fgets(linea2, 100, archivo);
+    
+    // Cerrar el archivo
+    fclose(archivo);
+    
+    // Mostrar las líneas leídas en la consola
+    printf("Línea 1: %s\n", linea1);
+    printf("Línea 2: %s\n", linea2);
+    
+    return 0;
 }
